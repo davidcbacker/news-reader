45170590176c71cd36b8801dafa055ccfeddde58
davidcbacker <20198171+davidcbacker@users.noreply.github.com>
2026-01-13 14:38:31 -0800

    Update rss_to_html.py

--- src/rss_to_html.py
+++ src/rss_to_html.py
@@ -109,8 +109,8 @@ def generate_html_base(title: str):
     <title>{title}</title>
     <link rel=\"stylesheet\" href=\"style.css\">
     <script src=\"script.js\"></script>
-</head>
     <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.cssn\">
+</head>
 <body>\n"""
     return html_base
 
